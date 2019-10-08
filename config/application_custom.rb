3f58d935947cd4002a2b423bdebb07b7b27b467d
Piotr Pęczek <piotr.peczak@raiffeisen.pl>
2019-10-08 19:47:17 +0200

    zoliborz only polish language

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,6 +1,6 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :pl
-    config.i18n.available_locales = [:pl, :en]
+    config.i18n.available_locales = [:pl]
   end
 end
